4ba0be6624dc33b1a15575235f2f1f384426e79c
Muneeb Ahmad <muneeb.panwar@gmail.com>
2020-03-16 08:54:51 -0400

    Apply validation validates_with uniqness formate inclution exclution

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,21 @@
+class StartWithAValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.name.starts_with? 'A'
+      record.errors[:name] << 'Need a name starting with A please!'
+    end
+  end
+end
 class Book < ApplicationRecord
-  validates :name, length: {in: 3..9, message: "This is not valid" }, presence: true,on: :create  
-    # format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-    # inclusion: { in: %w(ali aaa munee),message: "%{value} is not a valid size" }
+  validates_with StartWithAValidator
+  validates :name, presence: true, unless: Proc.new { |book| book.auther.blank? }
+  validates :name, length: {in: 3..9, message: "This is not valid" }, presence: true,
+    on: :create, 
+    confirmation: true, 
+    uniqueness: true,
+    numericality: { only_integer: true },
+    length: { minimum: 2 } ,length: { maximum: 500 },
+    format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" },
+    inclusion: { in: %w(ali aaa munee),message: "%{value} is not a valid size" },
+    exclusion: { in: %w(ali aaa munee),message: "%{value} is not a valid size" }
+    
 end
